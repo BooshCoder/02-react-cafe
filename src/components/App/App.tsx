d7f499a9cf5bfcd0802c9c8f4de1285392bf5155
Eugene <mypro4s@icloud.com>
2025-06-26 13:00:54 +0300

    ✨ Add vote logic and stats component

---
+++ src/components/App/App.tsx
@@ -0,0 +1,50 @@
+<div className={css.app}></div>
+
+import css from './App.module.css';
+import { CafeInfo } from '../CafeInfo/CafeInfo';
+import { VoteOption } from '../VoteOption/VoteOption';
+import { VoteStars } from '../VoteStars/VoteStats';
+
+
+
+import { useState } from 'react';
+import { Votes, VoteType } from '../../types/votes';
+
+export const App = () => {
+    const [votes, setVotes] = useState<Votes>({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  
+    const handleVote = (type: VoteType) => {
+      setVotes(prevVotes => ({
+        ...prevVotes,
+        [type]: prevVotes[type] + 1,
+      }));
+    };
+    const resetVotes = () => {
+        setVotes({
+          good: 0,
+          neutral: 0,
+          bad: 0,
+        });
+      };
+    const totalVotes = votes.good + votes.neutral + votes.bad;
+    const positiveRate = totalVotes === 0 ? 0 : Math.round((votes.good / totalVotes) * 100);
+    return (
+      <div className={css.app}>
+        <CafeInfo />
+        <VoteOption
+          onVote={handleVote}
+          onReset={resetVotes}
+          canReset={true}
+        />
+        <VoteStars
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />
+      </div>
+    );
+  };
